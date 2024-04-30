6724551b9a913e4f418502c8baa45579f6c466c4
m2106701 <m2106701@edu.misis.ru>
2024-04-30 18:18:44 +0300

    iis

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace ДЗ1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelDuration = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.comboItem = new System.Windows.Forms.ComboBox();
+            this.buttonPlot = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.labelNew = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Считать файл Ub(t)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelDuration
+            // 
+            this.labelDuration.BackColor = System.Drawing.SystemColors.Control;
+            this.labelDuration.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDuration.Location = new System.Drawing.Point(804, 30);
+            this.labelDuration.Name = "labelDuration";
+            this.labelDuration.Size = new System.Drawing.Size(334, 35);
+            this.labelDuration.TabIndex = 1;
+            this.labelDuration.Text = "Продолжительность";
+            this.labelDuration.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelName
+            // 
+            this.labelName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelName.Location = new System.Drawing.Point(289, 30);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(479, 35);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Имя файла";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboItem
+            // 
+            this.comboItem.FormattingEnabled = true;
+            this.comboItem.Items.AddRange(new object[] {
+            "Сигналы Ub(t)",
+            "Сигналы Ib(t)",
+            "Спектр сигнала",
+            "График p(t)",
+            "Кривые P(t), Q(t), S(t)",
+            "Гармоники(амплитуда)",
+            "Гармоники(фаза)"});
+            this.comboItem.Location = new System.Drawing.Point(26, 174);
+            this.comboItem.Name = "comboItem";
+            this.comboItem.Size = new System.Drawing.Size(204, 28);
+            this.comboItem.TabIndex = 3;
+            this.comboItem.SelectedIndexChanged += new System.EventHandler(this.comboItem_SelectedIndexChanged);
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Location = new System.Drawing.Point(289, 167);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(186, 40);
+            this.buttonPlot.TabIndex = 4;
+            this.buttonPlot.Text = "Построить график";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Считать файл Ib(t)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // labelNew
+            // 
+            this.labelNew.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelNew.ForeColor = System.Drawing.Color.Black;
+            this.labelNew.Location = new System.Drawing.Point(289, 89);
+            this.labelNew.Name = "labelNew";
+            this.labelNew.Size = new System.Drawing.Size(479, 38);
+            this.labelNew.TabIndex = 6;
+            this.labelNew.Text = "Имя файла";
+            this.labelNew.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(26, 242);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Navy;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(657, 322);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 606);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.labelNew);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonPlot);
+            this.Controls.Add(this.comboItem);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelDuration);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Добавить файл";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelDuration;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.ComboBox comboItem;
+        private System.Windows.Forms.Button buttonPlot;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label labelNew;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Data;
+using System.Linq;
+using MathNet.Numerics.IntegralTransforms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using FftSharp;
+
+namespace ДЗ1
+{
+    public partial class Form1 : Form
+    {
+        private string selectedElement;
+        private string fileName_u;
+        private string fileName_i;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName_u = openFileDialog.FileName;
+                ReadData(fileName_u);
+                //string shortFileName = Path.GetFileName(fileName);
+                labelName.Text = $"{fileName_u}";
+            } 
+        }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName_i = openFileDialog.FileName;
+                ReadData(fileName_i);
+                //string shortFileName = Path.GetFileName(fileName);
+                labelNew.Text = $"{fileName_i}";
+            }
+        }
+
+        public void ReadData(string fileName)
+        {
+            try
+            {
+                string[] oldLines = File.ReadAllLines(fileName);
+
+                string[] lines = new string [oldLines.Length / 2 +(oldLines.Length % 2 == 0 ? 0 : 1)];
+
+                for (int i = 0; i < oldLines.Length; i+=2)
+                {
+                    lines[i / 2] = oldLines[i];
+                    
+                }
+
+                double duration = lines.Length / 10;
+                TimeSpan timeSpan = TimeSpan.FromSeconds(duration);
+                string formattedTime = $"{(int)timeSpan.TotalHours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+                labelDuration.Text = $"Продолжительность: {formattedTime}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при чтении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void comboItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedElement = comboItem.SelectedItem.ToString();
+        }
+
+        private void buttonPlot_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+            if (string.IsNullOrEmpty(selectedElement))
+            {
+                MessageBox.Show("Выберите элемент в ComboBox.");
+                return;
+            }
+
+            string filePath1 = labelName.Text;
+            string filePath2 = labelNew.Text;
+            if (!File.Exists(filePath1)) //|| (!File.Exists(filePath2)))
+            {
+                MessageBox.Show("Файл не найден.");
+                return;
+            }
+
+            try
+            {
+                if ((selectedElement == "Сигналы Ib(t)") || (selectedElement == "Спектр сигнала"))
+                {
+                    fileName = fileName_i;
+                }
+                else if (selectedElement == "Сигналы Ub(t)")
+                {
+                    fileName = fileName_u;
+                }
+
+                string[] oldLines = File.ReadAllLines(fileName);
+                string[] lines = new string[oldLines.Length / 2 + (oldLines.Length % 2 == 0 ? 0 : 1)];
+                PlotSignal(oldLines);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при чтении файла: " + ex.Message);
+            }
+        }
+
+        private void PlotSignal(string[] lines)
+        {
+            if (lines.Length == 0)
+            {
+                MessageBox.Show("Файл пуст.");
+                return;
+            }
+
+            string lastLine = lines.Last();
+
+            string[] values = lastLine.Split('\t');
+
+            double[] signalData = new double[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+
+                if (!double.TryParse(values[i], out signalData[i]))
+                {
+                    MessageBox.Show($"Невозможно преобразовать значение \"{values[i]}\" в число.");
+                    return;
+                }
+            }
+
+            if ((selectedElement == "Сигналы Ub(t)") || (selectedElement == "Сигналы Ib(t)"))
+            {
+                chart1.Series.Clear();
+                Series series = new Series();
+                series.ChartType = SeriesChartType.Line;
+
+                double secondsPer = 0.1;
+                for (int i = 0; i < signalData.Length; i++)
+                {
+                    series.Points.AddXY(i * secondsPer, signalData[i]);
+                }
+
+                chart1.Series.Add(series);
+
+                chart1.ChartAreas[0].AxisX.Title = "Time (seconds)";
+                chart1.ChartAreas[0].AxisY.Title = "Signal Value";
+
+                chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = true;
+                chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
+
+                chart1.Legends.Add(new Legend());
+
+                chart1.Update();
+            }
+            else if (selectedElement == "Спектр сигнала")
+            {
+                int N = signalData.Length;
+                double delta_t = 0.0025;
+                double f = 50.0;
+                double delta_f = 0.015625 / delta_t;
+                double[] freq = new double[N];
+                System.Numerics.Complex[] result = new System.Numerics.Complex[N];
+                List<double> result_double = new List<double>();
+
+                double[] new_result = new double[64];
+                for (int i = 0; i < new_result.Length; i++)
+                {
+                    new_result[i] = signalData[i];
+                }
+
+                for (int k = 0; k < 64; k++)
+                {
+                    for (int n = 0; n < 64; n++)
+                    {
+                        double arg = -2 * Math.PI * k * n / 64;
+                        var complex = new System.Numerics.Complex(Math.Cos(-arg), Math.Sin(arg));               
+                        result[k] += signalData[n] * complex;
+
+                    }
+                }
+               
+                for (int i = 0; i < N; i++)
+                {
+                    freq[i] = i * delta_f; //x
+                }
+
+                System.Numerics.Complex[] fft = FftSharp.FFT.Forward(new_result);
+                List<double> mnim = new List<double> ();
+                foreach (var complexNumber in fft)
+                {
+                    double im = complexNumber.Imaginary;
+                    double rel = complexNumber.Real;
+                    double absmnim = Math.Sqrt(Math.Pow(im, 2) + Math.Pow(rel, 2));
+                    mnim.Add(absmnim);
+                }   
+
+                chart1.Series.Clear();
+                chart1.ChartAreas[0].AxisX.Title = "Частота";
+                chart1.ChartAreas[0].AxisY.Title = "Амплитуда";
+
+                Series series = new Series();
+                series.ChartType = SeriesChartType.Line;
+
+                for (int i = 0; i < mnim.Count; i++)
+                {
+                    series.Points.AddXY(freq[i], mnim[i]);
+                }
+
+                chart1.Series.Add(series);
+            }
+        }
+
+        
+
+    }
+}
